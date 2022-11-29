1d9ccebf31f6cc2af349ea2506a3af514abc18c1
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-11-29 15:22:12 +0100

    test

---
+++ src/components/App/App.js
@@ -0,0 +1,33 @@
+import Content from "components/Content/Content";
+import Header from "components/Header/Header";
+import Total from "components/Total/Total";
+import './App.css'
+
+
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 25;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        number1={exercises1}
+        part2={part2}
+        number2={exercises2}
+        part3={part3}
+        number3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </>
+  );
+};
+
+export default App;
